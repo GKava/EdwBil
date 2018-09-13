828bd22b0a3d3e47f334636af71c8fea7442b2f2
GKava <1devstack1@gmail.com>
2018-09-13 15:46:42 +0300

    all

---
+++ app/src/main/java/edwardbil_soundboard/gkain/edwardbil/MainActivity.java
@@ -0,0 +1,20 @@
+package edwardbil_soundboard.gkain.edwardbil;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.MotionEvent;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
