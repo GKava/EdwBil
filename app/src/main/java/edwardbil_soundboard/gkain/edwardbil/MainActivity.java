848f85406d5365ec8fc9f9c36147eac7ec6afc20
GKava <1devstack1@gmail.com>
2018-09-14 15:52:16 +0300

    saaaa

--- app/src/main/java/edwardbil_soundboard/gkain/edwardbil/MainActivity.java
+++ app/src/main/java/edwardbil_soundboard/gkain/edwardbil/MainActivity.java
@@ -1,20 +1,52 @@
 package edwardbil_soundboard.gkain.edwardbil;
 
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.view.MotionEvent;
 
-import java.util.ArrayList;
-import java.util.List;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+
 
 public class MainActivity extends AppCompatActivity {
+    public static FragmentManager fragmentManager;
+
+    private static final String TAG = "MainActivity";
+    private AdView mAdView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
 
+        fragmentManager = getSupportFragmentManager();
+        if (findViewById(R.id.fragment_container)!=null){
+            if (savedInstanceState!=null){
+                return;
+            }
+            fragmentManager.beginTransaction().add(R.id.fragment_container, new MainFragment()).commit();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mAdView.resume();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mAdView.destroy();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mAdView.pause();
     }
 }
